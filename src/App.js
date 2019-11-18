8bb70b115c6fc80d1f86884a595165bc9757e717
znha <zihna-19@hotmail.com>
2019-11-18 08:51:53 +0200

    creating the featured component and add the react-boostrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
 import "./App.css";
+import FeaturedArticle from "./components/FeaturedArticle";
 
 function App() {
-    return (<div className="App" >
-        Hello world!
-    </div>);
+    return <FeaturedArticle />;
 }
 
 export default App;
---
+++ src/components/FeaturedArticle.js
@@ -0,0 +1,10 @@
+import React from "react";
+class FeaturedArticle extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return "hello world";
+    }
+}
+export default FeaturedArticle;
