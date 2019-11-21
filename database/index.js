66ec7b08a4251c3ba8d4b8110a0cef8194e733cd
islamYMansour <islamy95@hotmail.com>
2019-11-21 14:49:48 +0200

    auth

--- database/index.js
+++ database/index.js
@@ -1,27 +1,28 @@
-const mongoose = require('../node_modules/mongoose/index.js');
-var User = require('./models/User').User;
-var Article = require('./models/Article').Article;
-var Category = require('./models/Category').Category;
+const mongoose = require("../node_modules/mongoose/index.js");
+var User = require("./models/User").User;
+var Article = require("./models/Article").Article;
+var Category = require("./models/Category").Category;
 
-const uri = "mongodb+srv://<user>:<passwrod>@cluster0-hft43.mongodb.net/mediunDB?retryWrites=true&w=majority";
+const uri =
+  "mongodb+srv://fatoom:fatoom@cluster0-hft43.mongodb.net/mediunDB?retryWrites=true&w=majority";
 
 mongoose
   .connect(uri, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    dbName: 'mediunDB'
+    dbName: "mediunDB"
   })
-  .catch((error) => console.log('this is error!', error));
+  .catch(error => console.log("this is error!", error));
 
 const { connection } = mongoose;
 
-connection.once('open', () => {
-  console.log('MongoDB database connection established successfully');
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
 });
 
 //need to be refactored
-const selectAll = function (obj, id, callback) {
-  obj.find({ id: id }, function (err, items) {
+const selectAll = function(obj, id, callback) {
+  obj.find({ id: id }, function(err, items) {
     if (err) {
       callback(err, null);
     } else {
@@ -30,62 +31,104 @@ const selectAll = function (obj, id, callback) {
   });
 };
 
-const getUsers = function (callback) {
-  User.find({}).
-    exec(function (err, user) {
-      if (err) { console.log(err) }
-      else { callback(user) }
-    })
+const getUsers = function(callback) {
+  User.find({}).exec(function(err, user) {
+    if (err) {
+      console.log(err);
+    } else {
+      callback(user);
+    }
+  });
 };
 
-const getFeatured = function (callback) {
+const getFeatured = function(callback) {
   Article.find({ clapsNumber: { $gte: 100 } })
     .limit(1)
-    .select('title subTitle pic createdAt readingTime categoryId clapsNumber authorId')
-    .exec(function (err, article) {
+    .select(
+      "title subTitle pic createdAt readingTime categoryId clapsNumber authorId"
+    )
+    .exec(function(err, article) {
       if (err) {
-        callback(err, null)
+        callback(err, null);
       } else {
         var articleAuthor;
-        getAuthor(User, article[0].authorId, function (err, author) {
+        getAuthor(User, article[0].authorId, function(err, author) {
           if (err) {
             console.log(err);
           } else {
-            var feauredArticle = { 'article': article[0], "articleAuthor": author }
-            console.log(feauredArticle);
-            callback(null, feauredArticle)
+            var feauredArticle = { article: article[0], articleAuthor: author };
+            // console.log(feauredArticle);
+            callback(null, feauredArticle);
           }
         });
-
       }
     });
+};
 
-}
-
-const getAuthor = function (model, authorId, callback) {
-  model.findOne({ id: authorId }).exec(function (err, user) {
+const getAuthor = function(model, authorId, callback) {
+  model.findOne({ id: authorId }).exec(function(err, user) {
     if (err) callback(err, null);
     callback(null, user);
   });
 };
 
-const getCategory = function (model, categoryId, callback) {
-  model.findOne({ id: categoryId }).exec(function (err, category) {
+const getCategory = function(model, categoryId, callback) {
+  model.findOne({ id: categoryId }).exec(function(err, category) {
     if (err) throw err;
     callback(category);
   });
 };
 
-const getLatest = function (callback) {
-  Article.find({}).
-    sort(['createdAt', 1]).
-    select('title subTitle pic createdAt readingTime categoryId clapsNumber authorId').
-    exec(function (err, article) {
-      if (err) { console.log(err) }
-      else { callback(article) }
+const getLatest = function(callback) {
+  Article.find({})
+    .sort(["createdAt", 1])
+    .select(
+      "title subTitle pic createdAt readingTime categoryId clapsNumber authorId"
+    )
+    .exec(function(err, article) {
+      if (err) {
+        console.log(err);
+      } else {
+        callback(article);
+      }
     });
-}
+};
+var signUp = function(user, callback) {
+  var userModel = new User({
+    id: new Date().getTime(),
+    name: user.name,
+    password: user.password,
+    email: user.email,
+    pic:
+      "https://cdn5.vectorstock.com/i/1000x1000/85/94/person-gray-photo-placeholder-man-silhouette-sign-vector-23838594.jpg"
+  });
+  User.findOne({ email: user.email }, (err, user) => {
+    if (user) {
+      callback("Exisistt");
+      return;
+    } else
+      userModel.save(err => {
+        if (err) {
+          console.log(err);
+          callback(err);
+        }
+        callback(null, user);
+      });
+  });
+};
 
+var getUser = function(user, callback) {
+  User.findOne({ email: user.email }).exec((err, data) => {
+    if (err) {
+      console.log(err);
+      callback(err);
+    } else {
+      // console.log(data);
+
+      callback(null, data);
+    }
+  });
+};
 
 module.exports.getAuthor = getAuthor;
 module.exports.selectAll = selectAll;
@@ -96,3 +139,5 @@ module.exports.Category = Category;
 module.exports.getLatest = getLatest;
 module.exports.Article = Article;
 module.exports.User = User;
+module.exports.signUp = signUp;
+module.exports.getUser = getUser;
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,10 @@
 const proxy = require("http-proxy-middleware");
-module.exports = function (app) {
-    app.use(
-        ["/articles/", "/user/login"],
-        proxy({
-            target: "http://localhost:3004",
-            changeOrigin: true
-        })
-    );
-
+module.exports = function(app) {
+  app.use(
+    ["/articles/", "/users/login"],
+    proxy({
+      target: "http://localhost:3004",
+      changeOrigin: true
+    })
+  );
 };
