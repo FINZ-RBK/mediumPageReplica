f7f2ba36ea4963f24b36c634af9b7b581219e63f
nuhla <nuhlamasri@gmail.com>
2019-11-21 20:17:08 +0200

    testing server end to end

--- server/testingServer.js
+++ server/testingServer.js
@@ -6,66 +6,78 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const port = process.env.PORT || 3004;
-app.get("/articles/getFeatured", function (req, res) {
-    console.log('reached ')
-    db.getFeatured(function (err, article) {
-        if (err) {
-            res.statusCode(504);
-        }
-        res.json(article);
-    });
-
+app.get("/articles/getFeatured", function(req, res) {
+  console.log("reached ");
+  db.getFeatured(function(err, article) {
+    if (err) {
+      res.statusCode(504);
+    }
+    res.json(article);
+  });
 });
 app.get("/articles/getUser", (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    // req.query.id
-    db.User.find({ id: req.query.id })
-        .select("name")
-        .then((name, err) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.send(name[0].name);
-            }
-        });
+  res.header("Access-Control-Allow-Origin", "*");
+  // req.query.id
+
+  db.User.find({ id: req.query.id })
+    .select("name")
+    .then((name, err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send({ name });
+      }
+    });
+});
+app.get("/articles/getCategory", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  console.log(res.header.toString(), "header");
+
+  db.Category.find({ id: req.query.id })
+    .select("name")
+    .then((categ, err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send({ categ });
+        // console.log(err);
+      }
+    });
 });
 
 app.get("/articles/getCategory", (req, res) => {
-    console.log("iam in category");
-    res.header("Access-Control-Allow-Origin", "*");
-    console.log(req.query.id);
-    db.Category.find({ id: req.query.id })
-        .select("name")
-        .then((categ, err) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log(categ.name);
-            }
-        });
+  console.log("iam in category");
+  res.header("Access-Control-Allow-Origin", "*");
+  console.log(req.query.id);
+  db.Category.find({ id: req.query.id })
+    .select("name")
+    .then((categ, err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(categ.name);
+      }
+    });
 });
+
 app.get("/articles/get10Articals", (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    console.log(req.query);
-    console.log("hi");
-    var AutherArray = req.query.id;
-    var Category = [];
-    var alldata = [];
+  res.header("Access-Control-Allow-Origin", "*");
+  var AutherArray = req.query.id;
+  var Category = [];
+  var alldata = [];
 
-    db.Article
-        .find({ id: { $gte: req.query.id } })
-        .sort({ id: 1 })
-        .limit(3)
-        .then(data => {
-            // console.log(data);
-            res.send(data);
-        });
+  db.Article.find({ id: { $gte: 84781 } })
+    .sort({ id: 1 })
+    .limit(3)
+    .then(data => {
+      res.send({ data });
+    });
 
-    //res.sendFile(path.resolve(__dirname, 'client', 'build', 'bundle.js'));
+  //res.sendFile(path.resolve(__dirname, 'client', 'build', 'bundle.js'));
 });
 
-app.post("/user/login", function (req, res) {
-    console.log('rescieve post request');
-    res.send('hello');
+app.post("/user/login", function(req, res) {
+  console.log("rescieve post request");
+  res.send("hello");
 });
 module.exports = app;
