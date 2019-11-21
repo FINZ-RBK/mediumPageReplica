991eae0eafb8066381c1b8f8fb5c174433a0c31e
znha <zihna-19@hotmail.com>
2019-11-21 17:12:39 +0200

    adding end to end test

--- src/server.test.js
+++ src/server.test.js
@@ -5,12 +5,19 @@ app.listen(4000);
 describe("testing server functions with database", () => {
     test("It should return an object", async (done) => {
         const response = await request(app).get("/articles/getFeatured");
-        expect(response.body).toBeDefined();
+        expect(response.body.article).toBeDefined();
+        expect(response.body.articleAuthor).toBeDefined();
         expect(response.statusCode).toBe(200);
         done();
     });
+    test("It should 404 for wrong requests", async (done) => {
+        const response = await request(app).get("/articles/nothing");
+        expect(response.statusCode).toBe(404);
+        done();
+    });
+});
 
-    // it("signup creates a new user record", function (done) {
+// it("signup creates a new user record", function (done) {
     //     var options = {
     //         method: "POST",
     //         uri: "http://localhost:3000/articles/getFeatured",
@@ -33,5 +40,3 @@ describe("testing server functions with database", () => {
     //         });
     //     });
     // });
-});
-
