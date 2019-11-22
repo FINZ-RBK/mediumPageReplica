0cfb8c0a11746195ffa0dfa5b0fb9f02cab86f3e
znha <zihna-19@hotmail.com>
2019-11-22 16:18:00 +0200

    tryin to figure out what is happening

--- src/index.js
+++ src/index.js
@@ -7,9 +7,14 @@ import App from './App';
 // import * as serviceWorker from './serviceWorker';
 import axios from 'axios';
 
+axios.get(`/articles/getFeatured`)
+    .then(res => {
+        // console.log('data from axios', res.data)
+        ReactDOM.render(<App farticle={res.data} />, document.getElementById("root"));
 
-ReactDOM.render(<App farticle={res.data} />, document.getElementById("root"));
-
+    }).catch(() => {
+        ReactDOM.render("Loading...", document.getElementById("root"));
+    });
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
