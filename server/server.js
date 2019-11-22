454b3ec159949dac522dfdbcf5c989555ff659b6
znha <zihna-19@hotmail.com>
2019-11-22 20:24:33 +0200

    trying to fix deployment

--- server/server.js
+++ server/server.js
@@ -178,7 +178,6 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('../build'));
     const path = require('path');
     app.get('*', (req, res) => {
-        res.send('hello', path.resolve(__dirname, '..', 'build', 'index.html'));
         res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
     })
 
