a29873ed249ced0b88017e9f0953689c9d897340
znha <zihna-19@hotmail.com>
2019-11-23 10:01:05 +0200

    solving auth problems

--- server/server.js
+++ server/server.js
@@ -24,149 +24,152 @@ app.use(cookieParser());
 // })
 
 app.use("*", (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
 
-  next();
+    next();
 });
-app.get("/articles/getFeatured", function(req, res) {
-  db.getFeatured(function(err, article) {
-    if (err) {
-      res.statusCode(504);
-    }
-    console.log(article);
+app.get("/articles/getFeatured", function (req, res) {
+    db.getFeatured(function (err, article) {
+        if (err) {
+            res.statusCode(504);
+        }
+        console.log(article);
 
-    res.json(article);
-  });
+        res.json(article);
+    });
 });
 
 app.get("/articles/getUser", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  db.User.find({ id: req.query.id })
-    .select("name")
-    .then((name, err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.send(name[0].name);
-      }
-    });
+    res.header("Access-Control-Allow-Origin", "*");
+    db.User.find({ id: req.query.id })
+        .select("name")
+        .then((name, err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(name[0].name);
+            }
+        });
 });
 
 app.get("/articles/getCategory", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  console.log(res.header.toString(), "header");
+    res.header("Access-Control-Allow-Origin", "*");
+    console.log(res.header.toString(), "header");
 
-  db.Category.find({ id: req.query.id })
-    .select("name")
-    .then((categ, err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(categ[0].name);
-        var data = categ[0].name;
-        res.send(data);
-        // console.log(err);
-      }
-    });
+    db.Category.find({ id: req.query.id })
+        .select("name")
+        .then((categ, err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(categ[0].name);
+                var data = categ[0].name;
+                res.send(data);
+                // console.log(err);
+            }
+        });
 });
 app.get("/articles/get10Articals", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  var AutherArray = req.query.id;
-  var Category = [];
-  var alldata = [];
+    res.header("Access-Control-Allow-Origin", "*");
+    var AutherArray = req.query.id;
+    var Category = [];
+    var alldata = [];
 
-  db.Article.find({ id: { $gte: req.query.id } })
-    .sort({ id: 1 })
-    .limit(3)
-    .then(data => {
-      res.send(data);
-    });
+    db.Article.find({ id: { $gte: req.query.id } })
+        .sort({ id: 1 })
+        .limit(3)
+        .then(data => {
+            res.send(data);
+        });
 
-  //res.sendFile(path.resolve(__dirname, 'client', 'build', 'bundle.js'));
+    //res.sendFile(path.resolve(__dirname, 'client', 'build', 'bundle.js'));
 });
 
 app.post(
-  "/users/signup",
-  [check("email").isEmail(), check("password").isLength({ min: 6 })],
-  async (req, res) => {
-    const errors = validationResult(req);
-    const accessToken = auth.generateAccessToken(req.body);
-    res.cookie(config.HEADER_AUTH, accessToken);
+    "/users/signup",
+    [check("email").isEmail(), check("password").isLength({ min: 6 })],
+    async (req, res) => {
+        const errors = validationResult(req);
+        const accessToken = auth.generateAccessToken(req.body);
+        res.cookie(config.HEADER_AUTH, accessToken);
 
-    if (!errors.isEmpty()) {
-      return res.status(203).json({ errors: errors.array() });
-    }
-    try {
-      const hashedPassword = await bcryptjs.hash(req.body.password, 10);
-      const user = {
-        name: req.body.name,
-        password: hashedPassword,
-        email: req.body.email
-      };
-      db.signUp(user, err => {
-        // try {
-        if (err) {
-          // throw err;
-          return res.status(201).send({ errors: [err] });
+        if (!errors.isEmpty()) {
+            return res.status(203).json({ errors: errors.array() });
         }
-        // } catch {}
+        try {
+            const hashedPassword = await bcryptjs.hash(req.body.password, 10);
+            const user = {
+                name: req.body.name,
+                password: hashedPassword,
+                email: req.body.email
+            };
+            db.signUp(user, err => {
+                // try {
+                if (err) {
+                    // throw err;
+                    return res.status(201).send({ errors: [err] });
+                }
+                // } catch {}
 
-        return res.status(200).send("sucess");
-        // res.send("success");
-      });
-    } catch {
-      return res.status(201).send({ errors: ["check your password please!"] });
+                return res.status(200).send("sucess");
+                // res.send("success");
+            });
+        } catch {
+            return res.status(201).send({ errors: ["check your password please!"] });
+        }
     }
-  }
 );
 
 app.post(
-  "/users/login",
-  [check("email").isEmail(), check("password").isLength({ min: 6 })],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(203).json({ errors: errors.array() });
-    }
-    var accessToken = auth.generateAccessToken(req.body);
-    res.cookie(config.HEADER_AUTH, accessToken);
-    db.getUser(req.body, async (err, user) => {
-      if (err) {
-        return res.status(201).send({ errors: ["not found"] });
-      }
-      try {
-        // console.log(user.password);
-        if (await bcryptjs.compare(req.body.password, user.password)) {
-          return res.status(200).send(user);
-        } else {
-          return res
-            .status(201)
-            .send({ errors: ["check your password please!"] });
+    "/users/login",
+    [check("email").isEmail(), check("password").isLength({ min: 6 })],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(203).json({ errors: errors.array() });
         }
-      } catch {
-        res.status(201).send({ errors: ["Error in Auth"] });
-      }
-    });
-  }
+        var accessToken = auth.generateAccessToken(req.body);
+        res.cookie(config.HEADER_AUTH, accessToken);
+        db.getUser(req.body, async (err, user) => {
+            if (err) {
+                return res.status(201).send({ errors: ["not found"] });
+            }
+            try {
+                // console.log(user.password);
+                if (await bcryptjs.compare(req.body.password, user.password)) {
+                    return res.status(200).send(user);
+                } else {
+                    return res
+                        .status(201)
+                        .send({ errors: ["check your password please!"] });
+                }
+            } catch {
+                res.status(201).send({ errors: ["Error in Auth"] });
+            }
+        });
+    }
 );
 
 app.get("/users/user", (req, res) => {
-  var token = req.headers[config.HEADER_AUTH];
-  if (token == null) return res.sendStatus(401);
-  try {
-    jwt.verify(token, config.ACCESS_TOKEN_SECRET, (err, user) => {
-      if (err) return res.sendStatus(403);
-      db.getUser(user, (err, user) => {
-        if (err) {
-          return res.status(400).send(err);
-        }
-        res.status(200).send(user);
-      });
-    });
-  } catch (err) {
-    res.status(401).json({ msg: "Token is not valid" });
-  }
+
+    var token = req.headers[config.HEADER_AUTH];
+    console.log(token);
+    if (token == null) return res.sendStatus(401);
+    try {
+        jwt.verify(token, config.ACCESS_TOKEN_SECRET, (err, user) => {
+            if (err) return res.sendStatus(403);
+            db.getUser(user, (err, user) => {
+                if (err) {
+                    return res.status(400).send(err);
+                }
+                console.log(user);
+                res.status(200).send(user);
+            });
+        });
+    } catch (err) {
+        res.status(401).json({ msg: "Token is not valid" });
+    }
 });
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve(__dirname, '..', 'build')));
@@ -176,5 +179,5 @@ if (process.env.NODE_ENV === 'production') {
 
 }
 app.listen(port, () => {
-  console.log("connected on port" + port);
+    console.log("connected on port" + port);
 });
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
 const proxy = require("http-proxy-middleware");
-module.exports = function(app) {
+module.exports = function (app) {
   app.use(
-    ["/articles/*", "/users/login", "/users/signup","/users/user"],
+    ["/articles/*", "/users/login", "/users/signup", "/users/user"],
     proxy({
       target: "http://localhost:3004",
       changeOrigin: true
