a75115990790cd5ebbec83ef6771b3ace026c88d
Fatoom Fayad <fatoomfayyad@gmail.com>
2020-01-26 17:57:57 +0200

    Update User.js

--- database/models/User.js
+++ database/models/User.js
@@ -3,6 +3,7 @@ var mongoose = require("mongoose");
 //GET the Schema constructor
 var Schema = mongoose.Schema;
 
+//User Schema
 const userSchema = new Schema({
     id: { type: Number, unique: true },
     name: { type: String },
@@ -11,6 +12,8 @@ const userSchema = new Schema({
     bio: { type: String }
   });
 
+//User model
 const User = mongoose.model('User', userSchema);
 
+//Export Model
 module.exports.User = User;
