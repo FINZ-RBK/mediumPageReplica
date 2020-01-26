bfb1c17e77ded0942167b98dc7b367fac7a6e44d
Fatoom Fayad <fatoomfayyad@gmail.com>
2020-01-26 18:21:08 +0200

    Update userSeeder.js

--- database/models/userSeeder.js
+++ database/models/userSeeder.js
@@ -1,11 +1,9 @@
 var faker = require ('faker');
 var db = require('../index');
 var User = require( './User').User;
-  // select the ids from the database for hte users .
+  // select the ids from the database for the users .
   for(i=0; i <=100; i++){
-    // var item = new Article()
-    //var item = ;
-    var user =new User({
+      var user =new User({
         id: Math.floor(Math.random() * (4000 - 2001 + 1)) + 2001,
         name: faker.name.firstName() + ' ' + faker.name.lastName(),
         pic: faker.internet.avatar(),
@@ -16,9 +14,7 @@ var User = require( './User').User;
       user.save(err =>{
         if(err) {
             console.log(err);
-        } else{
-            console.log('Hi');
-        }
+        } 
       }
 
       );
