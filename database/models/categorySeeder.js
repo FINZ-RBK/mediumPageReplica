38decd9fc51dd3ee6f81ce83571c19c7bfc59998
Fatoom Fayad <fatoomfayyad@gmail.com>
2020-01-26 18:18:59 +0200

    Update categorySeeder.js

--- database/models/categorySeeder.js
+++ database/models/categorySeeder.js
@@ -1,14 +1,13 @@
 var faker = require ('faker');
 var db = require('../index');
 var Category = require( './Category').Category;
-  // select the ids from the database for hte users .
+  // select the ids from the database for the users .
   for(i=0; i <=20; i++){
     var category =new Category({
         id: Math.floor(Math.random() * (100 - 1 + 1)) + 1,
         name: faker.random.word()
       });
-      console.log(category);
-      category.save(err =>{
+       category.save(err =>{
         if(err) {
             console.log(err);
         } else{
